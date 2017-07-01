f02a04a6d4cb1f2e195d0fdb58b776ef4f8ad293
Net <net@RMDs-iMac-108.local>
2017-07-01 09:35:30 +0530

    Initial Commit

---
+++ LoginPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginPage
+//
+//  Created by Net on 01/07/17.
+//  Copyright © 2017 Net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
